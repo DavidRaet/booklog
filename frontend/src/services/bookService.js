1e31b2a6a8c1c0ca73e6e52bdcb6831cd078aacf
David Raet <draet@vassar.edu>
2025-10-26 12:10:48 -0400

    feat: add frontend folder with React + Vite setup

---
+++ frontend/src/services/bookService.js
@@ -0,0 +1,17 @@
+const API_BASE_URL = "http://localhost:3001/api";
+
+
+export const bookService = {
+    getAllBooks: async () => {
+        const response = await fetch(`${API_BASE_URL}/books`);
+        if (!response.ok) throw new Error("Failed to fetch books.");
+        return response.json();
+    },
+
+    getBookById: async (id) => {
+        const response = await fetch(`${API_BASE_URL}/books/${id}`);
+        if (!response.ok) throw new Error("Book not found.");
+    }
+    
+
+}
