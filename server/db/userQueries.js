68693649c172c51c223747037b3891319bc24faf
David Raet <draet@vassar.edu>
2025-11-15 12:50:44 -0500

    feat: add a new user table to the database for 1 to many relationship with books. Also, currently integrating authentication using jwt and password hashing. Will add auth routes for signing up and logging in for users.

---
+++ server/db/userQueries.js
@@ -0,0 +1,42 @@
+import pool from "../database.js";
+import bcrypt from 'bcrypt'
+
+const SALT_ROUNDS = 10
+
+export const createUser = async (username, email, password) => {
+    const password_hash = bcrypt.hash(password, SALT_ROUNDS)
+
+    const result = await pool.query(
+        `INSERT INTO USERS (username, email, password_hash)
+         VALUES ($1, $2, $3)
+         RETURNING id, username, email, created_at
+        `,
+        [username, email, password_hash]
+    )
+
+    return result.rows[0]
+}
+
+
+export const getUserByEmail = async (email) => {
+    const result = await pool.query(`
+        SELECT * FROM users WHERE email = $1
+        `, [email]
+    )
+
+    return result.rows[0]
+}
+
+
+export const getUserById = async (id) => {
+    const result = await pool.query(`
+        SELECT id, username, email, created_at FROM users WHERE id = $1 
+        `, [id]
+    )
+
+    return result.rows[0]
+}
+
+export const confirmPassword = async (plainPassword, hashedPassword) => {
+    return await bcrypt.compare(plainPassword, hashedPassword)
+}
---
+++ server/middleware/auth.js
@@ -0,0 +1,19 @@
+import verifyToken from '../utils/jwt.js'
+
+
+export const authenticateToken = (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1]
+
+    if (!token){
+        return res.status(401).json({ message: 'Access token required' })
+    }
+
+    const decoded = verifyToken(token)
+
+    if (!decoded){
+        return res.status(403).json({ message: 'Invalid or expired token' })
+    }
+
+    req.userId = decoded.userId
+    next()
+}
